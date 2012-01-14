3f587a18a6393c6df114043c51de387fe0bab35d
Ju-hyung Lee <jufoot@gmail.com>
2012-01-14 13:58:12 +0900

    3d vector & 3x3 matrix added

--- math.js
+++ math.js
@@ -99,6 +99,7 @@ vec2.prototype.dot = function(v) {
     return this.x * v.x + this.y * v.y;
 }
 
+// z-component of 3d cross product (ax, ay, 0) x (bx, by, 0)
 vec2.prototype.cross = function(v) {
     return this.x * v.y - this.y * v.x;
 }
@@ -205,14 +206,139 @@ vec2.truncate = function(v, length) {
 }
 
 //-----------------------------------
+// 3D Vector
+//-----------------------------------
+
+function vec3(x, y, z) {
+    this.x = x || 0;
+    this.y = y || 0;
+    this.z = z || 0;
+}
+
+vec3.zero = new vec3(0, 0, 0);
+
+vec3.prototype.set = function(x, y, z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+
+    return this;
+}
+
+vec3.prototype.copy = function(v) {
+    this.x = v.x;
+    this.y = v.y;
+    this.z = v.z;
+    
+    return this;
+}
+
+vec3.prototype.duplicate = function() {
+    return new vec3(this.x, this.y, this.z);
+}
+
+vec3.prototype.add = function(v1, v2) {
+    this.x = v1.x + v2.x;
+    this.y = v1.y + v2.y;
+    this.z = v1.z + v2.z;
+
+    return this;
+}
+
+vec3.prototype.addself = function(v) {
+    this.x += v.x;
+    this.y += v.y;
+    this.z += v.z;
+
+    return this;
+}
+
+vec3.prototype.sub = function(v1, v2) {
+    this.x = v1.x - v2.x;
+    this.y = v1.y - v2.y;
+    this.z = v1.z - v2.z;
+
+    return this;
+}
+
+vec3.prototype.subself = function(v) {
+    this.x -= v.x;
+    this.y -= v.y;
+    this.z -= v.z;
+
+    return this;
+}
+
+vec3.prototype.scale = function(s) {
+    this.x *= s;
+    this.y *= s;
+    this.z *= s;
+
+    return this;
+}
+
+vec3.prototype.mad = function(v, s) {
+    this.x += v.x * s;
+    this.y += v.y * s;
+    this.z += v.z * s;
+}
+
+vec3.prototype.neg = function() {
+    this.x *= -1;
+    this.y *= -1;
+    this.z *= -1;
+
+    return this;
+}
+
+vec3.prototype.lengthsq = function() {
+    return this.x * this.x + this.y * this.y + this.z * this.z;
+}
+
+vec3.prototype.length = function() {
+    return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
+}
+
+vec3.prototype.normalize = function() {
+    var inv = (this.x != 0 || this.y != 0 || this.z != 0) ? 1 / Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z) : 0;
+    this.x *= inv;
+    this.y *= inv;
+    this.z *= inv;
+
+    return this;
+}
+
+vec3.prototype.dot = function(v) {
+    return this.x * v.x + this.y * v.y + this.z * v.z;
+}
+
+vec3.prototype.toVec2 = function() {
+    return new vec2(this.x, this.y);
+}
+
+vec3.fromVec2 = function(v, z) {
+    return new vec3(v.x, v.y, z);
+}
+
+vec3.truncate = function(v, length) {
+    var ret = v.duplicate();
+    var length_sq = v.x * v.x + v.y * v.y + v.z * v.z;
+    if (length_sq > length * length) {
+        ret.scale(length / Math.sqrt(length_sq));
+    }
+
+    return ret;
+}
+
+//-----------------------------------
 // 2x2 Matrix (row major)
 //-----------------------------------
 
 function mat2(_11, _12, _21, _22) {
-    this._11 = _11;
-    this._12 = _12;
-    this._21 = _21;
-    this._22 = _22;
+    this._11 = _11 || 0;
+    this._12 = _12 || 0;
+    this._21 = _21 || 0;
+    this._22 = _22 || 0;
 }
 
 mat2.zero = new mat2(0, 0, 0, 0);
@@ -239,42 +365,6 @@ mat2.prototype.duplicate = function() {
     return new mat2(this._11, this._12, this._21, this._22);
 }
 
-mat2.prototype.add = function(m1, m2) {
-    this._11 = m1._11 + m2._11;
-    this._12 = m1._12 + m2._12;
-    this._21 = m1._21 + m2._21;
-    this._22 = m1._22 + m2._22;
-
-    return this;
-}
-
-mat2.prototype.addself = function(m) {
-    this._11 += m._11;
-    this._12 += m._12;
-    this._21 += m._21;
-    this._22 += m._22;
-
-    return this;
-}
-
-mat2.prototype.sub = function(m1, m2) {
-    this._11 = m1._11 - m2._11;
-    this._12 = m1._12 - m2._12;
-    this._21 = m1._21 - m2._21;
-    this._22 = m1._22 - m2._22;
-
-    return this;
-}
-
-mat2.prototype.subself = function(m) {
-    this._11 -= m._11;
-    this._12 -= m._12;
-    this._21 -= m._21;
-    this._22 -= m._22;
-
-    return this;
-}
-
 mat2.prototype.scale = function(s) {
     this._11 *= s;
     this._12 *= s;
@@ -284,8 +374,189 @@ mat2.prototype.scale = function(s) {
     return this;
 }
 
+mat2.prototype.mul = function(m) {
+    return this.set(
+        this._11 * m2._11 + this._12 * m2._21,
+        this._11 * m2._12 + this._12 * m2._22,
+        this._21 * m2._11 + this._22 * m2._21,
+        this._21 * m2._12 + this._22 * m2._22);
+}
+
 mat2.prototype.mulvec = function(v) {
-    return new vec2(this._11 * v.x + this._12 * v.y, this._21 * v.x + this._22 * v.y);
+    return new vec2(
+        this._11 * v.x + this._12 * v.y, 
+        this._21 * v.x + this._22 * v.y);
+}
+
+mat2.prototype.invert = function() {
+    var det = this._11 * this._22 - this._12 * this._21;
+    if (det != 0)
+        det = 1 / det;
+    
+    return this.set(
+        this._22 * det, -this._12 * det,
+        -this._21 * det, this._11 * det);   
+}
+
+// solve A * x = b
+mat2.prototype.solve = function(b) {
+    var det = this._11 * this._22 - this._12 * this._21;
+    if (det != 0) 
+        det = 1 / det;
+    
+    return new vec2(
+        det * (this._22 * b.x - this._12 * b.y), 
+        det * (this._11 * b.y - this._21 * b.x));
+}
+
+mat2.mul = function(m1, m2) {
+    return new mat2(
+        m1._11 * m2._11 + m1._12 * m2._21,
+        m1._11 * m2._12 + m1._12 * m2._22,
+        m1._21 * m2._11 + m1._22 * m2._21,
+        m1._21 * m2._12 + m1._22 * m2._22);
+}
+
+//-----------------------------------
+// 3x3 Matrix (row major)
+//-----------------------------------
+
+function mat3(_11, _12, _13, _21, _22, _23, _31, _32, _33) {
+    this._11 = _11 || 0;
+    this._12 = _12 || 0;
+    this._13 = _13 || 0;
+    this._21 = _21 || 0;
+    this._22 = _22 || 0;
+    this._23 = _23 || 0;
+    this._31 = _31 || 0;
+    this._32 = _32 || 0;
+    this._33 = _33 || 0;
+}
+
+mat3.zero = new mat3(0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+mat3.prototype.set = function(_11, _12, _13, _21, _22, _23, _31, _32, _33) {
+    this._11 = _11;
+    this._12 = _12;
+    this._13 = _13;
+    this._21 = _21;
+    this._22 = _22;
+    this._23 = _23;
+    this._31 = _31;
+    this._32 = _32;
+    this._33 = _33;
+
+    return this;
+}
+
+mat3.prototype.copy = function(m) {
+    this._11 = m._11;
+    this._12 = m._12;
+    this._13 = m._13;
+    this._21 = m._21;
+    this._22 = m._22;
+    this._23 = m._23;
+    this._31 = m._31;
+    this._32 = m._32;
+    this._33 = m._33;
+
+    return this;
+}
+
+mat3.prototype.duplicate = function() {
+    return new mat3(this._11, this._12, this._13, this._21, this._22, this._23, this._31, this._32, this._33);
+}
+
+mat3.prototype.scale = function(s) {
+    this._11 *= s;
+    this._12 *= s;
+    this._13 *= s;
+    this._21 *= s;
+    this._22 *= s;
+    this._23 *= s;
+    this._31 *= s;
+    this._32 *= s;
+    this._33 *= s;
+
+    return this;
+}
+
+mat3.prototype.mul = function(m) {
+    return this.set(
+        this._11 * m2._11 + this._12 * m2._21 + this._13 * m2._31,
+        this._11 * m2._12 + this._12 * m2._22 + this._13 * m2._32,
+        this._11 * m2._13 + this._12 * m2._23 + this._13 * m2._33,
+        this._21 * m2._11 + this._22 * m2._21 + this._23 * m2._31,
+        this._21 * m2._12 + this._22 * m2._22 + this._23 * m2._32,
+        this._21 * m2._13 + this._22 * m2._23 + this._23 * m2._33,
+        this._31 * m2._11 + this._32 * m2._21 + this._33 * m2._31,
+        this._31 * m2._12 + this._32 * m2._22 + this._33 * m2._32,
+        this._31 * m2._13 + this._32 * m2._23 + this._33 * m2._33);
+}
+
+mat3.prototype.mulvec = function(v) {
+    return new vec2(
+        this._11 * v.x + this._12 * v.y + this._13 * v.z, 
+        this._21 * v.x + this._22 * v.y + this._23 * v.z,
+        this._31 * v.x + this._32 * v.y + this._33 * v.z);
+}
+
+mat3.prototype.invert = function() {
+    var det2_11 = this._22 * this._33 - this._23 * this._32;
+    var det2_12 = this._23 * this._31 - this._21 * this._33;
+    var det2_13 = this._21 * this._32 - this._22 * this._31;
+
+    var det = this._11 * det2_11 + this._12 * det2_12 + this._13 * det2_13;
+    if (det != 0)
+        det = 1 / det;
+
+    var det2_21 = this._13 * this._32 - this._12 * this._33;    
+    var det2_22 = this._11 * this._33 - this._13 * this._31;
+    var det2_23 = this._12 * this._31 - this._11 * this._32;
+    var det2_31 = this._12 * this._23 - this._13 * this._22;
+    var det2_32 = this._13 * this._21 - this._11 * this._23;    
+    var det2_33 = this._11 * this._22 - this._12 * this._21;
+
+    return this.set(
+        det2_11 * det, det2_12 * det, det2_13 * det,
+        det2_21 * det, det2_22 * det, det2_23 * det,
+        det2_31 * det, det2_32 * det, det2_33 * det);
+}
+
+// solve A * x = b
+mat3.prototype.solve = function(b) {
+    var det2_11 = this._22 * this._33 - this._23 * this._32;
+    var det2_12 = this._23 * this._31 - this._21 * this._33;
+    var det2_13 = this._21 * this._32 - this._22 * this._31;
+
+    var det = this._11 * det2_11 + this._12 * det2_12 + this._13 * det2_13;
+    if (det != 0)
+        det = 1 / det;
+
+    var det2_21 = this._13 * this._32 - this._12 * this._33;    
+    var det2_22 = this._11 * this._33 - this._13 * this._31;
+    var det2_23 = this._12 * this._31 - this._11 * this._32;
+    var det2_31 = this._12 * this._23 - this._13 * this._22;
+    var det2_32 = this._13 * this._21 - this._11 * this._23;    
+    var det2_33 = this._11 * this._22 - this._12 * this._21;
+    
+    return new vec3(
+        det * (det2_11 * b.x + det2_12 * b.y + det2_13 * b.z),
+        det * (det2_21 * b.x + det2_22 * b.y + det2_23 * b.z),
+        det * (det2_31 * b.x + det2_32 * b.y + det2_33 * b.z));
+}
+
+mat3.mul = function(m1, m2) {
+    return new mat3(
+        m1._11 * m2._11 + m1._12 * m2._21 + m1._13 * m2._31,
+        m1._11 * m2._12 + m1._12 * m2._22 + m1._13 * m2._32,
+        m1._11 * m2._13 + m1._12 * m2._23 + m1._13 * m2._33,
+        m1._21 * m2._11 + m1._22 * m2._21 + m1._23 * m2._31,
+        m1._21 * m2._12 + m1._22 * m2._22 + m1._23 * m2._32,
+        m1._21 * m2._13 + m1._22 * m2._23 + m1._23 * m2._33,
+        m1._31 * m2._11 + m1._32 * m2._21 + m1._33 * m2._31,
+        m1._31 * m2._12 + m1._32 * m2._22 + m1._33 * m2._32,
+        m1._31 * m2._13 + m1._32 * m2._23 + m1._33 * m2._33);
 }
 
 //-----------------------------------
