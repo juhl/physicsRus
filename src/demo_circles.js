42aed9a1ff404820a7da21f1b6c804007f9a46a2
Ju-hyung Lee <jufoot@gmail.com>
2012-03-08 20:52:49 +0900

    Changed elasticity.

--- src/demo_circles.js
+++ src/demo_circles.js
@@ -13,8 +13,8 @@ DemoCircles = function() {
 		for (var i = 0; i < 25; i++) {
 			var body = new Body(Body.DYNAMIC, new vec2(Math.random() * 20 - 10, 0.5 + Math.random() * 14));
 			var shape = new ShapeCircle(0, 0, 1.4 * (Math.max(Math.random(), 0.2)));
-			shape.e = 0.1;
-			shape.u = 1.0;
+			shape.e = 0.8;
+			shape.u = 0.9;
 			shape.density = 1;
 			body.addShape(shape);
 			body.resetMassData();
